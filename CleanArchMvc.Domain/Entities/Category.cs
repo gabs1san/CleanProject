b17053127d7b00327f11e698f065bcdcafff9a24
gabs1san <sanbiel398@gmail.com>
2025-11-19 20:31:48 -0300

    Criando as entidades de dominio

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
